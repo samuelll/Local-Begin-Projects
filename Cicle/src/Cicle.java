6276880e6bd742ed629692fbfa5d6721723e368f
Yatsuma <Yatsuma@192.168.1.3>
2016-03-17 16:48:37 +0200

    Cicle

---
+++ Cicle/src/Cicle.java
@@ -0,0 +1,12 @@
+
+public class Cicle {
+	public static void main(String args[]){
+		int[][] nume = {{1,2,3},{4,5,6},{7,8,9}};
+		for(int row=0; row<nume.length; row++){
+			for(int col=0; col<nume[row].length; col++){
+		System.out.print(nume[row][col]+"  ");
+			}
+		System.out.println();
+		}
+		}
+	}
